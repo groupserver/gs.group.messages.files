85c6e158867ee4d11f3fec5c39897938f70fe9ab
Michael JasonSmith <mpj17@onlinegroups.net>
2013-11-21 12:54:43 -0800

    Dealing with missing files better.

--- gs/group/messages/files/browser/javascript/files.js
+++ gs/group/messages/files/browser/javascript/files.js
@@ -1,6 +1,14 @@
+// GroupServer JavaScript module for providing the Search mechanism
+//
+// Copyright © 2013 OnlineGroups.net and Contributors.
+// All Rights Reserved.
+//
+// This software is subject to the provisions of the Zope Public License,
+// Version 2.1 (ZPL). http://groupserver.org/downloads/license/
+//
 jQuery.noConflict();
 function gs_group_messages_files_init_home() {
-    var filesSearch = null, b = null, url = null;
+    var filesSearch=null, b=null, url=null;
 
     b = jQuery('base').attr('href');
     if (b[b.length -1] != '/') {
@@ -14,6 +22,6 @@ function gs_group_messages_files_init_home() {
 }
 
 jQuery(window).load(function () {
-    gsJsLoader.with_module('/++resource++gs-search-base-js-min-20121217.js',
+    gsJsLoader.with_module('/++resource++gs-search-base-js-min-20131121.js',
                            gs_group_messages_files_init_home);
 });
