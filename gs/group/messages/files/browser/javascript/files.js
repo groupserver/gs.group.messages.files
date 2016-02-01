ed31f2a79e382588f5a4c970d5d8e8f2707f316a
Michael JasonSmith <mpj17@onlinegroups.net>
2016-02-01 11:27:28 -0800

    Mostly a whitespace cleanup so gjslint runs without error

--- gs/group/messages/files/browser/javascript/files.js
+++ gs/group/messages/files/browser/javascript/files.js
@@ -1,7 +1,7 @@
-"use strict";
+'use strict';
 // GroupServer JavaScript module for providing the Search mechanism
 //
-// Copyright © 2013, 2014 OnlineGroups.net and Contributors.
+// Copyright © 2013, 2014, 2016 OnlineGroups.net and Contributors.
 // All Rights Reserved.
 //
 // This software is subject to the provisions of the Zope Public License,
@@ -9,20 +9,20 @@
 //
 jQuery.noConflict();
 function gs_group_messages_files_init_home() {
-    var filesSearch=null, b=null, url=null;
+    var filesSearch = null, b = null, url = null;
 
     b = jQuery('base').attr('href');
-    if (b[b.length -1] != '/') {
+    if (b[b.length - 1] != '/') {
         b = b + '/';
     }
     url = b + 'gs-group-messages-files-ajax.html';
 
-    filesSearch = GSSearch('#gs-group-messages-files-search', url, 0, 5, {}, 
+    filesSearch = GSSearch('#gs-group-messages-files-search', url, 0, 5, {},
                            null);
     filesSearch.load();
 }
 
-jQuery(window).load(function () {
+jQuery(window).load(function() {
     gsJsLoader.with_module('/++resource++gs-search-base-js-min-20140313.js',
                            gs_group_messages_files_init_home);
 });
